ecf1a6485c932498aa20c5fc6b784fe46caa0b46
younis hamalawy <younis.hamalawy@gmail.com>
2017-06-23 14:36:12 -0700

    Completed code for user specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,86 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  subject(:user) { User.create(username: "Adam", password: "password") }
+
+  describe "GET #new" do
+    it "renders the new user page" do
+      get :new
+      expect(response).to render_template("new")
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid information" do
+      it "creates a new user" do
+        post :create, user: { username: "Tom", password: "password" }
+        expect(response).to render_template("show")
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "with no password" do
+      it "should stay on the new user page and show error" do
+        post :create, user: { username: "Tom", password: "" }
+        expect(response).to render_template("new")
+        expect(response).to have_http_status(:error)
+        expect(page).to have_content("Password can't be blank")
+      end
+    end
+
+    context "with no username" do
+      it "should stay on the new user page and show error" do
+        post :create, user: { username: "", password: "password" }
+        expect(response).to render_template("new")
+        expect(response).to have_http_status(:error)
+        expect(page).to have_content("Username can't be blank")
+      end
+    end
+  end
+
+  describe "GET #edit" do
+    it "renders the edit page" do
+      get :edit, id: user.id
+      expect(response).to render_template("edit")
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "PATCH #update" do
+    context "with valid information" do
+      it "updates the users information" do
+        patch :update, id: user.id, user: { username: "Tom", password: "password" }
+        expect(response).to render_template("show")
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "with invalid information" do
+      it "stays on edit page and shows error" do
+        patch :update, id: user.id, user: { username: "Tom", password: "" }
+        expect(response).to render_template("edit")
+        expect(response).to have_http_status(:error)
+        expect(page).to have_content("Password can't be blank")
+      end
+    end
+  end
+
+  # describe "GET #index" do
+  #   it "renders the user index page"
+  # end
+
+  describe "GET #show" do
+    it "renders the user page" do
+      get :show, id: user.id
+      expect(response).to render_template("show")
+      expect(response).to have_http_status(200)
+      expect(page).to have_content(user.username)
+    end
+  end
+
+  # describe "DELETE #destroy" do
+  #   it "deletes the user"
+  # end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,41 +7,71 @@ RSpec.describe User, type: :model do
   it { should validate_length_of(:password).is_at_least(6) }
 
   # Association tests
+  subject(:user) { User.create(username: "Adam", password: "password") }
 
   describe "session_token" do
-    context "#generate_seesion_token" do
-      it "should be 16 characters long"
+    context "::generate_seesion_token" do
+      it "should be 16 characters long" do
+        expect(User::generate_seesion_token.length).to eq(16)
+      end
     end
 
     context "#reset_session_token!" do
-      it "should reset the users session token"
+      it "should reset the users session token" do
+        old_token = user.session_token
+        user.reset_session_token!
+        expect(user.session_token).not_to eq(old_token)
+      end
     end
 
     context "#ensure_session_token" do
-      it "should set session token if not set"
-      it "should return same session token if set"
+      it "should set session token if not set" do
+        user.session_token = nil
+        user.reset_session_token!
+        expect(user.session_token).not_to be_nil
+      end
+
+      it "should return same session token if set" do
+        old_token = user.session_token
+        user.reset_session_token!
+        expect(user.session_token).to eq(old_token)
+      end
     end
   end
 
   describe "password" do
-    it "doesn't store the plaintext password in the user"
+    it "doesn't store the plaintext password in the user" do
+      expect(user.password).to be_nil
+    end
 
     context "#password=" do
-      it "set the password digest for the user"
+      it "set the password digest for the user" do
+        old_password_digest = user.password_digest
+        user.password = "otherpassword"
+        expect(user.password_digest).not_to eq(old_password_digest)
+      end
     end
 
     context "#is_password?" do
-      it "should check the password matches the digest"
+      it "should check the password matches the digest" do
+        expect(user.is_password?("password")).to be_true
+        expect(user.is_password?("notpassword")).to be_false
+      end
     end
   end
 
   describe "#find_by_credentials" do
     context "with valid credentials" do
-      it "should return the user"
+      it "should return the user" do
+        expect(User.find_by_credentials("Adam", "password")).to eq(user)
+      end
     end
 
     context "with invalid credentials" do
-      it "should return nil"
+      it "should return nil" do
+        expect(User.find_by_credentials("Adam", "notpassword")).to be_nil
+        expect(User.find_by_credentials("not_Adam", "password")).to be_nil
+      end
     end
   end
 end
