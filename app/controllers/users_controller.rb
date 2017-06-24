7c587a0ebe05446da07323b31ca8fed8b5b4539c
younis hamalawy <younis.hamalawy@gmail.com>
2017-06-23 17:00:59 -0700

    Write helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # login!(@user)
+      login!(@user)
       redirect_to user_url(@user)
     else
       flash[:errors] = @user.errors.full_messages
